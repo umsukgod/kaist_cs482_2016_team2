57828c54931f5ba93e588293d0a6004b5cde2af6
bcue250 <umsukgod@hanmail.net>
2016-09-29 22:29:13 +0900

    Made a new empty project

---
+++ Need_a_Light/app/src/main/java/com/example/need_a_light/Need_a_Light.java
@@ -0,0 +1,13 @@
+package com.example.need_a_light;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Need_a_Light extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_need_a__light);
+    }
+}
